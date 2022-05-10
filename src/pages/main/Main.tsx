a98e61c4b058396ac14914a6fe5770c6c9d2dbf9
sig03 <sig03@naver.com>
2022-05-10 09:18:42 +0000

    Add test logic - Add signin logic - Divide signin & main page

---
+++ src/pages/main/Main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Main() {
+  return (
+	  <div>aaa</div>
+  );
+}
+
+export default Main
---
+++ src/pages/signin/SignIn.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function SignIn() {
+  return (
+    <>
+      <div>
+        <input type='text' name='email' />
+        <input type='password' name='password' />
+        <button>Sign In</button>
+      </div>
+    </>
+  )
+}
+
+export default SignIn
