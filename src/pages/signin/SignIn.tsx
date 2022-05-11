7803032879e1c67241d90dcbbfc3f8e379dab1a3
sig03 <sig03@naver.com>
2022-05-11 13:43:43 +0000

    Add signin css

--- src/pages/signin/SignIn.tsx
+++ src/pages/signin/SignIn.tsx
@@ -3,10 +3,21 @@ import React from 'react';
 function SignIn() {
   return (
     <>
-      <div>
-        <input type='text' name='email' />
-        <input type='password' name='password' />
-        <button>Sign In</button>
+      <div className='sign-in-wrapper'>
+        <div className='sign-in-box'>
+          <div>
+            <h1>로그인</h1>
+          </div>
+          <div>
+            <input type='text' name='email' className=''/>
+          </div>
+          <div>
+            <input type='password' name='password' className=''/>
+          </div>
+          <div>
+            <button>로그인</button>
+          </div>
+        </div>
       </div>
     </>
   )
