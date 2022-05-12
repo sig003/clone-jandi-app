ddd0d3ef32408a14f36852745e47d9aff2dfeb2c
sig03 <sig03@naver.com>
2022-05-12 14:11:39 +0000

    Modify sign in css

--- src/pages/signin/SignIn.tsx
+++ src/pages/signin/SignIn.tsx
@@ -9,10 +9,10 @@ function SignIn() {
             <h1>로그인</h1>
           </div>
           <div>
-            <input type='text' name='email' className=''/>
+            <input type='text' name='email' className='sign-in-input' placeholder='이메일' />
           </div>
           <div>
-            <input type='password' name='password' className=''/>
+            <input type='password' name='password' className='sign-in-input' placeholder='비밀번호' />
           </div>
           <div>
             <button>로그인</button>
