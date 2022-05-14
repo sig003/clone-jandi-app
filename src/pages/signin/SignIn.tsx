5cb7732e19e2995f60dd4c161df7a8435378dbec
sig03 <sig03@naver.com>
2022-05-14 01:01:24 +0000

    modify signin page

--- src/pages/signin/SignIn.tsx
+++ src/pages/signin/SignIn.tsx
@@ -3,31 +3,45 @@ import React from 'react';
 function SignIn() {
   return (
     <>
-      <div className='sign-in-wrapper'>
-        <div className='sign-in-box'>
-          <div>
-            <h1>로그인</h1>
-          </div>
-          <div>
-            <input type='text' name='email' className='sign-in-input' placeholder='이메일' />
-          </div>
-          <div>
-            <input type='password' name='password' className='sign-in-input' placeholder='비밀번호' />
-          </div>
-          <fieldset>
+      <header>
+
+      </header>
+      <main>
+        <div className='sign-in-wrapper'>
+          <div className='sign-in-box'>
             <div>
-              <input type='checkbox' />
-              <label>이메일 기억하기</label>
-              <input type='checkbox' />
-              <label>로그인 상태유지</label>
-              <a>비밀번호 찾기</a>
+              <h1>로그인</h1>
             </div>
-          </fieldset>
-          <div>
-            <button>로그인</button>
+            <div>
+              <input type='text' name='email' className='sign-in-input' placeholder='이메일' />
+            </div>
+            <div>
+              <input type='password' name='password' className='sign-in-input' placeholder='비밀번호' />
+            </div>
+            <fieldset>
+              <div className='sign-in-form'>
+                <div>
+                  <input type='checkbox' />
+                  <label>
+                    <span>이메일 기억하기</span>
+                  </label>
+                  <input type='checkbox' />
+                  <label>
+                    <span>로그인 상태유지</span>
+                  </label>
+                </div>
+                <div>
+                  <a>비밀번호 찾기</a>
+                </div>
+              </div>
+            </fieldset>
+            <div>
+              <button>로그인</button>
+            </div>
+            <section>잔디가 처음이신가요? 회원가입</section>
           </div>
         </div>
-      </div>
+      </main>
     </>
   )
 }
