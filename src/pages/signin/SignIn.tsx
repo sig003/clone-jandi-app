daace31af725648a21e40e6c9f1d078009f99e2e
sig03 <sig03@naver.com>
2022-05-15 07:49:16 +0000

    Add signin css

--- src/pages/signin/SignIn.tsx
+++ src/pages/signin/SignIn.tsx
@@ -4,7 +4,22 @@ function SignIn() {
   return (
     <>
       <header>
-
+        <nav>
+          <div>
+            logo jandi
+          </div>
+          <div>
+            잔디소개
+            고객사례
+            요금안내
+            도입문의
+            블로그
+리포트
+이벤트
+다운로드
+로그인
+          </div>
+        </nav>
       </header>
       <main>
         <div className='sign-in-wrapper'>
@@ -20,25 +35,28 @@ function SignIn() {
             </div>
             <fieldset>
               <div className='sign-in-form'>
-                <div>
+                <div className='sign-in-round-checkbox'>
                   <input type='checkbox' id='rememberEmail' />
-                  <label for='rememberEmail'>
+                  <label htmlFor='rememberEmail'>
                     <span>이메일 기억하기</span>
                   </label>
-                  <input type='checkbox' />
-                  <label>
+                  <input type='checkbox' id='maintainLogin' />
+                  <label htmlFor='maintainLogin'>
                     <span>로그인 상태 유지</span>
                   </label>
                 </div>
                 <div>
-                  <a>비밀번호 찾기</a>
+                  <a>
+                    비밀번호 찾기
+                    <i className='sign-in-arrow' />
+                  </a>
                 </div>
               </div>
             </fieldset>
             <div>
               <button>로그인</button>
             </div>
-            <section>잔디가 처음이신가요? <span>회원가입</span></section>
+            <section>잔디가 처음이신가요? <a href=''>회원가입</a></section>
           </div>
         </div>
       </main>
