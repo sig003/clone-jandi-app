7aaf598ad168c661dc9b2aca894e818c0f52152e
sig03 <sig03@naver.com>
2022-05-19 07:58:28 +0000

    Add main page css

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 function Main() {
   return (
-	  <div>aaa</div>
+	  <div className='main-wrapper'>
+      <div className='main-left'>left</div>
+      <div className='main-center'>center</div>
+      <div className='main-right'>right</div>
+    </div>
   );
 }
 
