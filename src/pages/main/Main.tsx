1d1081f64e94a7ca198f75ed17e6398b832d38ef
sig03 <sig03@naver.com>
2022-05-20 14:20:56 +0000

    Add left menu drag and resize

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 function Main() {
+  const sidebarRef = useRef(null);
+  const [isResizing, setIsResizing] = useState(false);
+  const [sidebarWidth, setSidebarWidth] = useState(268);
+
+  const startResizing = React.useCallback((mouseDownEvent) => {
+    setIsResizing(true);
+  }, []);
+
+  const stopResizing = React.useCallback(() => {
+    setIsResizing(false);
+  }, []);
+
+  const resize = React.useCallback(
+    (mouseMoveEvent) => {
+      if (isResizing) {
+        setSidebarWidth(
+          mouseMoveEvent.clientX -
+            sidebarRef.current.getBoundingClientRect().left
+        );
+      }
+    },
+    [isResizing]
+  );
+
+  useEffect(() => {
+    window.addEventListener("mousemove", resize);
+    window.addEventListener("mouseup", stopResizing);
+    return () => {
+      window.removeEventListener("mousemove", resize);
+      window.removeEventListener("mouseup", stopResizing);
+    };
+  }, [resize, stopResizing]);
+
   return (
 	  <div className='main-wrapper'>
-      <div className='main-left'>left</div>
+      <div 
+        className='main-left'
+        ref={sidebarRef}
+        style={{ width: sidebarWidth }}
+        onMouseDown={(e) => e.preventDefault()}
+      >
+        <div className='main-left-content'>left</div>
+        <div className='main-left-resizer' onMouseDown={startResizing} />
+      </div>
       <div className='main-center'>center</div>
       <div className='main-right'>right</div>
     </div>
