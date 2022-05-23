d31e191bef8285ab1d5a524004baa2d2e2bd3ff7
sig03 <sig03@naver.com>
2022-05-23 08:56:34 +0000

    modifying right, center

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -44,37 +44,45 @@ function Main() {
         <div className='main-left-content'>left</div>
         <div className='main-left-resizer' onMouseDown={startResizing} />
       </div>
-      <div className='main-center'>center</div>
+      <div className='main-center'>
+        <div className='main-center-odaeri'></div>
+      </div>
       <div className='main-right'>
-        <div className='main-right-icon-wrapper'>
-          <i className='main-right-menu-setting'></i>
+        <div className='main-right-is-top'>
+          <ul className='main-right-icon-top-wrapper'>
+            <li className='main-right-icon-circle'>
+              <i className='main-right-menu-setting'></i>
+            </li>
+            <li className='main-right-icon-circle'>
+              <i className='main-right-menu-search'></i>
+            </li>
+            <li className='main-right-icon-circle'>
+              <i className='main-right-menu-todo'></i>
+            </li>
+            <li className='main-right-icon-circle'>
+              <i className='main-right-menu-file'></i>
+            </li>
+            <li className='main-right-icon-circle'>
+              <i className='main-right-menu-origanization'></i>
+            </li>
+            <li className='main-right-icon-circle'>
+              <i className='main-right-menu-drive'></i>
+            </li>
+            <li className='main-right-icon-circle'>
+              <i className='main-right-menu-connect'></i>
+            </li>
+          </ul>
         </div>
-        <div className='main-right-icon-wrapper'>
-          <i className='main-right-menu-search'></i>
-        </div>
-        <div className='main-right-icon-wrapper'>
-          <i className='main-right-menu-todo'></i>
-        </div>
-        <div className='main-right-icon-wrapper'>
-          <i className='main-right-menu-file'></i>
-        </div>
-        <div className='main-right-icon-wrapper'>
-          <i className='main-right-menu-origanization'></i>
-        </div>
-        <div className='main-right-icon-wrapper'>
-          <i className='main-right-menu-drive'></i>
-        </div>
-        <div className='main-right-icon-wrapper'>
-          <i className='main-right-menu-connect'></i>
-        </div>
-        <div className='main-right-icon-wrapper'>
-          <i className='main-right-menu-favorite'></i>
-        </div>
-        <div className='main-right-icon-wrapper'>
-          <i className='main-right-menu-mention'></i>
-        </div>
-        <div className='main-right-icon-wrapper'>
-          <i className='main-right-menu-profile'></i>
+        <div className='main-right-is-bottom'>
+          <div className='main-right-icon-circle'>
+            <i className='main-right-menu-favorite'></i>
+          </div>
+          <div className='main-right-icon-circle'>
+            <i className='main-right-menu-mention'></i>
+          </div>
+          <div className='main-right-icon-circle'>
+            <i className='main-right-menu-profile'></i>
+          </div>
         </div>
       </div>
     </div>
