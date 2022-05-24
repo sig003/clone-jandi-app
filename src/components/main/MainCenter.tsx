a9ff7401d118228aaececd629bbbe3457ce745de
sig03 <sig03@naver.com>
2022-05-24 19:43:46 +0000

    modifying center/left css

--- src/components/main/MainCenter.tsx
+++ src/components/main/MainCenter.tsx
@@ -7,7 +7,7 @@ function MainCenter() {
         <div className="main-center-odaeri"></div>
         <p className="main-center-entry-message">
           <span>오늘은 어디에서 시작해볼까요?</span>
-          프로젝트별 특정 주제의 그룹 대화는 '토픽'
+          프로젝트별 특정 주제의 그룹 대화는 '토픽' <br />
           자유로운 1:1 또는 그룹 대화는 '채팅'
         </p>
       </div>
