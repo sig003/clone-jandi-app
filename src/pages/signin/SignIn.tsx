eb37b14e728ca08ae0c63a5a15355533e3d70350
sig03 <sig03@naver.com>
2022-05-31 02:34:10 +0000

    adding signin process

--- src/pages/signin/SignIn.tsx
+++ src/pages/signin/SignIn.tsx
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 
 function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onChangeEmail = useCallback(e => {
+    setEmail(e.target.value);
+  },[email]);
+
+  const onChangePassword = useCallback(e => {
+    setPassword(e.target.value);
+  },[password]);
+
+  const handleClickFormSubmit = () => {
+    console.log(`${email} === ${password}`);
+  }
+
   return (
     <>
       <header>
@@ -32,43 +47,63 @@ function SignIn() {
         </nav>
       </header>
       <main>
-        <div className="sign-in-wrapper">
-          <div className="sign-in-box">
-            <div>
-              <h1>로그인</h1>
-            </div>
-            <div>
-              <input type="text" name="email" className="sign-in-input" placeholder="이메일" />
-            </div>
-            <div>
-              <input type="password" name="password" className="sign-in-input" placeholder="비밀번호" />
-            </div>
-            <fieldset>
-              <div className="sign-in-form">
-                <div className="sign-in-round-checkbox">
-                  <input type="checkbox" id="rememberEmail" />
-                  <label htmlFor="rememberEmail">
-                    <span>이메일 기억하기</span>
-                  </label>
-                  <input type="checkbox" id="maintainLogin" />
-                  <label htmlFor="maintainLogin">
-                    <span>로그인 상태 유지</span>
-                  </label>
-                </div>
-                <div>
-                  <a>
-                    비밀번호 찾기
-                    <i className="sign-in-arrow" />
-                  </a>
-                </div>
+        <form>
+          <div className="sign-in-wrapper">
+            <div className="sign-in-box">
+              <div>
+                <h1>로그인</h1>
               </div>
-            </fieldset>
-            <div>
-              <button>로그인</button>
+              <div>
+                <input 
+                  type="text"
+                  name="email"
+                  className="sign-in-input"
+                  placeholder="이메일"
+                  onChange={onChangeEmail}
+                  value={email}
+                />
+              </div>
+              <div>
+                <input 
+                  type="password"
+                  name="password"
+                  className="sign-in-input"
+                  placeholder="비밀번호"
+                  onChange={onChangePassword}
+                  value={password}
+                />
+              </div>
+              <fieldset>
+                <div className="sign-in-form">
+                  <div className="sign-in-round-checkbox">
+                    <input type="checkbox" id="rememberEmail" />
+                    <label htmlFor="rememberEmail">
+                      <span>이메일 기억하기</span>
+                    </label>
+                    <input 
+                      type="checkbox"
+                      id="maintainLogin"
+                      
+                    />
+                    <label htmlFor="maintainLogin">
+                      <span>로그인 상태 유지</span>
+                    </label>
+                  </div>
+                  <div>
+                    <a>
+                      비밀번호 찾기
+                      <i className="sign-in-arrow" />
+                    </a>
+                  </div>
+                </div>
+              </fieldset>
+              <div>
+                <button type="submit" onClick={handleClickFormSubmit}>로그인</button>
+              </div>
+              <section>잔디가 처음이신가요? <a href="">회원가입</a></section>
             </div>
-            <section>잔디가 처음이신가요? <a href="">회원가입</a></section>
           </div>
-        </div>
+        </form>
       </main>
     </>
   )
